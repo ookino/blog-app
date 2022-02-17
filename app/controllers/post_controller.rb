2b4ff7c6d5b53b6e6acbba2790471fb8a4c818c3
Yaseer Anaisi Okino <37089367+yaseerokino@users.noreply.github.com>
2022-02-17 01:51:37 +0100

    Add controllers for users and posts

---
+++ app/controllers/post_controller.rb
@@ -0,0 +1,7 @@
+class PostController < ApplicationController
+   def list
+   end
+   
+   def show
+   end
+end
---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,7 @@
+class UserController < ApplicationController
+   def list
+   end
+   
+   def show
+   end
+end
