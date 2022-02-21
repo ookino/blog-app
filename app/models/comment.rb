3fc7a2a2def55ade15a5c380ec0203d3735c0381
Yaseer Anaisi Okino <37089367+yaseerokino@users.noreply.github.com>
2022-02-21 10:10:50 +0100

    Add associations for comment, post and user models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,4 @@
 class Comment < ApplicationRecord
+  belongs_to :author, foreign_key: 'User'
+  belongs_to :post, foreign_key: 'post_id'
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  belongs_to :author, foreign_key: 'User'
+  has_many :comments, foreign_key: 'post_id'
+  has_many :likes, foreign_key: 'post_id'
 end
