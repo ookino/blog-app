5c1606afec1fd0c8bc508e830fa39ae7c0cfd3c8
Yaseer Anaisi Okino <37089367+yaseerokino@users.noreply.github.com>
2022-02-26 13:34:53 +0100

    Made required changes

--- db/migrate/20220226112406_update_user_id_column_likes.rb
+++
@@ -1,5 +0,0 @@
-class UpdateUserIdColumnLikes < ActiveRecord::Migration[7.0]
-  def change
-    remove_index :likes, :post_id
-  end
-end
--- db/migrate/20220226114207_update_post_id_column_likes.rb
+++
@@ -1,5 +0,0 @@
-class UpdatePostIdColumnLikes < ActiveRecord::Migration[7.0]
-  def change
-    remove_index :likes, :user_id
-  end
-end
