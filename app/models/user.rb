859f81a9dc74bf27a868bb2d993151b069fbb5d0
Yaseer Anaisi Okino <37089367+yaseerokino@users.noreply.github.com>
2022-03-05 16:39:28 +0100

    Fix linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'user_id'
 
   def recent_3_posts
-   posts.limit(3).order(created_at: :desc)
+    posts.limit(3).order(created_at: :desc)
   end
 end
